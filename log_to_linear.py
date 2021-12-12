0f89bc1e6fd9b0913814f3b7b2f1f544285fed08
hina0830g <78065514+hina0830g@users.noreply.github.com>
2021-12-11 18:46:51 -0700

    Update log_to_linear.py

--- log_to_linear.py
+++ log_to_linear.py
@@ -13,16 +13,13 @@ a = -0.08 # lower uncertainty
 b = 0.07 # upper uncertainty
 c = -0.5 # Value
 #====================================#
-Bot = 10**(a+c)
-Up = 10**(b+c)
-val = 10**(c)
+Bot = 10**(a+c)# Evaluates lower error in linear scale
+Up = 10**(b+c) # Evaluates upper error in linear scale
+val = 10**(c) # Evaluates values in linear scale
 error1 = Up-val
 error2 = Bot-val
 
 print('upper',  '{:.3f}'.format(Up))
-print('act. value: ',  '{:.3f}'.format(val))
-#print('lower:',  '{:.3f}'.format(error1))
+print('actual value: ',  '{:.3f}'.format(val))
 print('lower:',  '{:.3f}'.format(Bot))
 print('value is: ',  '{:.3f}'.format(val), '+' + '{:.3f}'.format(error1), 'or', '{:.3f}'.format(error2))
-
-print(np.log10(1.59*10**(-8)))
